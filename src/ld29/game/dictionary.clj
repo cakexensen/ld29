9b4b0373b8041ba3045b17244a5b5cddea1a1ca7
xensky <xensky@gmail.com>
2014-04-26 12:14:06 -0400

    adding terms to dictionary

--- src/ld29/game/dictionary.clj
+++ src/ld29/game/dictionary.clj
@@ -7,4 +7,14 @@
   (make-command-dictionary
    [:look ["look" | "view" | "examine" | "inspect" | "check" "out" | "perceive"]]
    [:look-at [:look | "look" "at"]]
-   [:get ["get" | "take" | "acquire" | "attain"]]))
+   [:get ["get" | "take" | "acquire" | "attain"]]
+   [:school ["school" | "seahorses" | "school" "of" "seahorses" | "seahorse"]]
+   [:seal ["seal" | "silly" "seal"]]
+   [:open ["open" | :look "inside"]]
+   [:box ["box" | "cardboard" "box"]]
+   [:open-door [:open :door | "turn" :door-knob | "open" :door-knob]]
+   [:door ["door" | "portal" | "doorway"]]
+   [:door-knob ["door" "knob" | "door" "handle" | "knob" | "handle"]]
+   [:ship ["ship" | "sunken" "ship" | "ship" "ruin" | "sunken" "ship" "ruin" | "ship" "ruins" | "sunken" "ship" "ruins"]]
+   [:toilet-brush ["toilet" "brush" | "brush"]]
+   [:drain ["drain" | "toilet" "drain"]]))
