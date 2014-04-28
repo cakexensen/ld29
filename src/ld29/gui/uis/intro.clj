1881f29f15a01e4279ab4728665957f570abf667
xensky <xensky@gmail.com>
2014-04-28 16:21:01 -0400

    improve intro screen

--- src/ld29/gui/uis/intro.clj
+++ src/ld29/gui/uis/intro.clj
@@ -1,8 +1,14 @@
 (ns ld29.gui.uis.intro
+  (:import [com.badlogic.gdx.scenes.scene2d.ui Label])
   (:use [ld29.gui.uis.core]))
 
 (defn render
   "renders the game state"
   [stage state]
-  (let [message (:animated-message state)]
+  (let [intro (Label. "Introduction" @style)
+        prompt (Label. "Press enter..." @style)
+        message (:animated-message state)]
+    (.addActor @stage prompt)
+    (.setY intro first-line-y)
+    (.addActor @stage intro)
     (format-message message stage)))
