507d18480abffed127dbb9f1a338f85275c38603
xensky <xensky@gmail.com>
2014-04-28 13:56:42 -0400

    move game-overs from up to wiz

--- src/ld29/game/areas/wiz.clj
+++ src/ld29/game/areas/wiz.clj
@@ -10,8 +10,14 @@
    (= begged 1) "*fart* \"You sure? It's a long swim up to the surface.\" *fart*"
    (= begged 2) "\"If I do this, you won't be able to come back.\" *fart*"
    (= begged 3) "\"I'm gonna ask one more time.\" *fart* \"Are you absolutely sure you want to lose your gills?\""
-   (>= begged 4) ["\"Well, okay then.\" The wizard raises his hands, farts, and speaks a magic incantation. You feel your gills receding, and you suddenly cannot breathe. \"You better start swimming to the surface now. Get a move on!\" He kicks you upward, and you hear one last fart before you begin your ascent."
-                  (move-player :up)]))
+   (>= begged 4) (let [wiz-msg "\"Well, okay then.\" The wizard raises his hands, farts, and speaks a magic incantation. You feel your gills receding, and you suddenly cannot breathe. \"You better start swimming to the surface now. Get a move on!\" He kicks you upward, and you hear one last fart before you begin your ascent. "]
+                   (cond
+                    (get-game-state  :slime)
+                    (game-over (str wiz-msg "Just as you thought you were about to escape with your life, a slimy and ravenous shark-man with splinters in his teeth slithers up to you at incredible speed. You try to swim away, but he catches you and latches onto your leg. He shakes you and grinds his teeth slowly deeper into your flesh until your shredded tissue floats off, revealing a nearly clean bone before he goes for the rest of your limbs in turn. Your final score was 2."))
+                    (get-game-state  :rescue)
+                    (game-over (str wiz-msg "You're returned to your true form, but at what cost? You think of Marlune as you breathe the first breath of the rest of your journey above the surface. Your final score was 7. "))
+                    :else
+                    (game-over (str wiz-msg "You couldn't find it in your heart to care for the merfolk after all. There were even more idiots down below than there ever were up here. If you had to do it all again, you'd probably bring something more lethal than a toilet brush. Your final score was 0. "))))))
 
 (defentity wizard
   ""
