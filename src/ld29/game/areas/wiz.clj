d9eb0dc0448de95209192383dfdc20eaedfb5fe5
xensky <xensky@gmail.com>
2014-04-28 20:30:42 -0400

    fix wizard game over actions

--- src/ld29/game/areas/wiz.clj
+++ src/ld29/game/areas/wiz.clj
@@ -6,18 +6,23 @@
 (defn gill-removal-dialogue
   [begged]
   (cond
-   (= begged 0) "\"Really? Why'd you wanna go and\" *fart* \"do a thing like that? I can remove them, but I don't think it's a good idea.\""
-   (= begged 1) "*fart* \"You sure? It's a long swim up to the surface.\" *fart*"
-   (= begged 2) "\"If I do this, you won't be able to come back.\" *fart*"
-   (= begged 3) "\"I'm gonna ask one more time.\" *fart* \"Are you absolutely sure you want to lose your gills?\""
-   (>= begged 4) (let [wiz-msg "\"Well, okay then.\" The wizard raises his hands, farts, and speaks a magic incantation. You feel your gills receding, and you suddenly cannot breathe. \"You better start swimming to the surface now. Get a move on!\" He kicks you upward, and you hear one last fart before you begin your ascent. "]
-                   (cond
-                    (get-game-state  :slime)
-                    (game-over (str wiz-msg "Just as you thought you were about to escape with your life, a slimy and ravenous shark-man with splinters in his teeth slithers up to you at incredible speed. You try to swim away, but he catches you and latches onto your leg. He shakes you and grinds his teeth slowly deeper into your flesh until your shredded tissue floats off, revealing a nearly clean bone before he goes for the rest of your limbs in turn. Your final score was 2."))
-                    (get-game-state  :rescue)
-                    (game-over (str wiz-msg "You're returned to your true form, but at what cost? You think of Marlune as you breathe the first breath of the rest of your journey above the surface. Your final score was 7. "))
-                    :else
-                    (game-over (str wiz-msg "You couldn't find it in your heart to care for the merfolk after all. There were even more idiots down below than there ever were up here. If you had to do it all again, you'd probably bring something more lethal than a toilet brush. Your final score was 0. "))))))
+   (= begged 0)
+   "\"Really? Why'd you wanna go and\" *fart* \"do a thing like that? I can remove them, but I don't think it's a good idea.\""
+   (= begged 1)
+   "*fart* \"You sure? It's a long swim up to the surface.\" *fart*"
+   (= begged 2)
+   "\"If I do this, you won't be able to come back.\" *fart*"
+   (= begged 3)
+   "\"I'm gonna ask one more time.\" *fart* \"Are you absolutely sure you want to lose your gills?\""
+   (>= begged 4)
+   (let [wiz-msg "\"Well, okay then.\" The wizard raises his hands, farts, and speaks a magic incantation. You feel your gills receding, and you suddenly cannot breathe. \"You better start swimming to the surface now. Get a move on!\" He kicks you upward, and you hear one last fart before you begin your ascent. "]
+     (cond
+      (get-game-state  :slime)
+      (game-over (str wiz-msg "Just as you thought you were about to escape with your life, a slimy and ravenous shark-man with splinters in his teeth slithers up to you and grabs your leg. He eats you. Your final score was 2."))
+      (get-game-state  :rescue)
+      (game-over (str wiz-msg "You're returned to your true form, but at what cost? You think of Marlune as you breathe the first breath of the rest of your journey above the surface. Your final score was 7. "))
+      :else
+      (game-over (str wiz-msg "You couldn't find it in your heart to care for the merfolk after all. There were even more idiots down below than there ever were up here. If you had to do it all again, you'd probably bring something more lethal than a toilet brush. Your final score was 0. "))))))
 
 (defentity wizard
   ""
@@ -38,16 +43,16 @@
                    "*fart* \"Could you please take some slime from the sunken ship and spread it around the sharkman's hideout?\" *fart* \"He's been really getting on my nerves lately!\""
                    :else
                    "\"I don't think I mentioned a favour, but since you're interested,\" *fart* \"take some slime from the sunken ship and spread it around the sharkman's hideout. He's been really getting on my nerves lately!\"")]
-                 [[:ask :about :removing :gills | :ask :wizard :about :removing :gills]
+                 [[:ask :gills | :ask :about :removing :gills | :ask :wizard :about :removing :gills]
                   (let [begged (get-entity-state :wizard :begged)]
-                    (gill-removal-dialogue begged)
-                    (set-entity-state :wizard :begged (inc begged)))]
-                 [[:beg :about :removing :gills | :beg :wizard :about :removing :gills | :ask :nicely :about :removing :gills]
+                    [(gill-removal-dialogue begged)
+                     (set-entity-state :wizard :begged (inc begged))])]
+                 [[:beg :gills | :beg :about :removing :gills | :beg :wizard :about :removing :gills | :ask :nicely :about :removing :gills]
                   (let [begged (get-entity-state :wizard :begged)
                         ; inc begged more because you begged
                         begged (inc begged)]
-                    (gill-removal-dialogue begged)
-                    (set-entity-state :wizard :begged (inc begged)))])
+                    [(gill-removal-dialogue begged)
+                     (set-entity-state :wizard :begged (inc begged))])])
   ; begged is a vague measure of how much begging the player has done
   ; it increases more when using words like "beg" instead of just "ask"
   {:begged 0})
